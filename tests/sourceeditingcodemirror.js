2bb9f64031bb42be3d26faf464fc9cb6edd5e5ec
Christopher C. Wells <git-2022@chris-wells.net>
2022-09-11 20:06:51 -0700

    Update tests

--- tests/sourceeditingcodemirror.js
+++ tests/sourceeditingcodemirror.js
@@ -2,9 +2,15 @@ import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
 import SourceEditingCodeMirror from '../src/sourceeditingcodemirror';
 
 /* global document */
+
+import { global } from 'ckeditor5/src/utils';
+import CodeMirror from 'codemirror/src/edit/CodeMirror';
+
+global.window.CodeMirror = CodeMirror;
 
 describe( 'SourceEditingCodeMirror', () => {
 	it( 'should be named', () => {
@@ -23,6 +29,7 @@ describe( 'SourceEditingCodeMirror', () => {
 					Paragraph,
 					Heading,
 					Essentials,
+					SourceEditing,
 					SourceEditingCodeMirror
 				]
 			} );
@@ -36,7 +43,7 @@ describe( 'SourceEditingCodeMirror', () => {
 		it( 'should load SourceEditingCodeMirror', () => {
 			const SourceEditingCodeMirror = editor.plugins.get( 'SourceEditingCodeMirror' );
 
-			expect( SourceEditingCodeMirror ).to.be.an.instanceof( SourceEditingCodeMirror );
+			expect( SourceEditingCodeMirror ).to.be.an.instanceof( SourceEditingCodeMirror.constructor );
 		} );
 	} );
 } );
